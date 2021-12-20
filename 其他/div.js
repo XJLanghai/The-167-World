ad1b1696ebb12f7a03e42302b68120b5959ea60c
Tinhone <tinhone23333@gmail.com>
2021-12-21 07:22:31 +0800

    更新了文案、功能以及样式
    修改错字及文案；添加切换页面自动转至页首功能；更改部分样式使其更加清晰

--- 其他/div.js
+++ 其他/div.js
@@ -1,7 +1,9 @@
-var aa = ["a1","a2","a3","a4","a5"];
+var aa = ["a1", "a2", "a3", "a4", "a5"];
+$('body,html').animate({ scrollTop: 0 }, 0);//×ªµ½Ò³Ê×
 function a(b) {
-	for (var i = aa.length-1; i >= 0; i--) {
-		document.getElementById(aa[i]).style.display="none";
+	for (var i = aa.length - 1; i >= 0; i--) {
+		$('body,html').animate({ scrollTop: 0 }, 0);
+		document.getElementById(aa[i]).style.display = "none";
 	}
-	document.getElementById(aa[b]).style.display="";
+	document.getElementById(aa[b]).style.display = "";
 }
